2a732646efef951a23da406ba37c36891fdcd1ff
ismihan-ozer <98219075+ismihan-ozer@users.noreply.github.com>
2024-10-09 15:58:04 +0300

    Add files via upload

---
+++ Views/Home/destinations.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TatilSitesi.Views.Home
+{
+    public class destinationsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
